fa23cd41958793fac8c6a4a05a3de6362b162a53
amir soltnai <soltani@gmail.com>
2023-11-27 07:01:28 +0330

    register assets

--- Core.php
+++ Core.php
@@ -31,7 +31,7 @@ class Core
         add_action('wp_enqueue_scripts', [$this, 'wp_vip_register_assets']);
         add_action('admin_enqueue_scripts', [$this, 'wp_vip_register_assets_admin']);
     }
-    private function wp_vip_register_assets()
+    public function wp_vip_register_assets()
     {
         // CSS
         wp_register_style('vip-style', VIP_PLUGIN_URL . '/assets/css/front/front-style.css', '', '1.0.0');
@@ -41,11 +41,22 @@ class Core
         wp_register_script('vip-main-js', VIP_PLUGIN_URL . '/assets/js/front/main.js', ['jquery'], '1.0.0', true);
         wp_enqueue_script('vip-main-js');
     }
-    private function wp_vip_register_assets_admin()
+    public function wp_vip_register_assets_admin()
     {
+        // UIkit CSS
+        wp_register_style('uikit-style', 'https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/css/uikit.min.css', '', '1.0.0');
+        wp_enqueue_style('uikit-style');
+
         // CSS
-        wp_register_style('vip-style', VIP_PLUGIN_URL . '/assets/css/front/front-style.css', '', '1.0.0');
-        wp_enqueue_style('vip-style');
+        wp_register_style('vip-style-admin', VIP_PLUGIN_URL . '/assets/css/admin/admin.css', '', '1.0.0');
+        wp_enqueue_style('vip-style-admin');
+
+        // UIkit JS
+        wp_register_script('uikit-js', 'https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit.min.js', ['jquery'], '1.0.0', true);
+        wp_enqueue_script('uikit-js');
+
+        wp_register_script('uikit-icon-js', 'https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit-icons.min.js', ['jquery'], '1.0.0', true);
+        wp_enqueue_script('uikit-icon-js');
 
         // JS
         wp_register_script('vip-admin-js', VIP_PLUGIN_URL . '/assets/js/admin/admin.js', ['jquery'], '1.0.0', true);
@@ -58,3 +69,4 @@ class Core
     {
     }
 }
+$core = new Core(); 
